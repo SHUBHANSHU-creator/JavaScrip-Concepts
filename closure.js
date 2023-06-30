c2cc961b3ab12850f3ceb7063af45eddc1d4eec2
Shubhanshu Kumar <shukumarbham0@gmail.com>
2023-06-30 23:24:40 +0530

    origin

--- closure.js
+++ closure.js
@@ -3,15 +3,39 @@
 //   A block is also treated as a scope since ES6. 
 
 
-function foo() {
-    let b = 1;
-    function inner() {
-        return b++;
-    }
-    return inner;
-}
-let get_func_inner = foo();
+// function foo() {
+//     let b = 1;
+//     function inner() {
+//         return b++;
+//     }
+//     return inner;
+// }
+// let get_func_inner = foo();
  
-console.log(get_func_inner());
-console.log(get_func_inner());
-console.log(get_func_inner());
+// console.log(get_func_inner());
+// console.log(get_func_inner());
+// console.log(get_func_inner());
+
+
+function cart(){
+    let _count = 0
+    function updateCount(val){
+        _count += val
+    }
+    return{
+        increament(){
+            updateCount(1)
+        },
+        decreament(){
+            updateCount(-1)
+        },
+        value(){
+            return _count
+        }
+    }
+}
+
+let counter = cart()
+counter.increament()
+counter.increament()
+console.log(counter.value());
